77831536289e83ae97e432c66c6f55637eca389e
awiseman-carto <33461575+awiseman-carto@users.noreply.github.com>
2018-04-05 11:47:41 -0400

    trying new data that supposedly works

--- js/main.js
+++ js/main.js
@@ -4,17 +4,19 @@ window.onload = setMap();
 //set up choropleth map
 function setMap(){
     //use queue to parallelize asynchronous data loading
+    console.log("waiting")
     d3.queue()
-        .defer(d3.csv, "data/bicyclerisk.csv") //load attributes from csv
-        .defer(d3.json, "data/states.topojson") //load background spatial data
-        .defer(d3.json, "data/tracts.topojson") //load choropleth spatial data
+        .defer(d3.csv, "data/state_province.csv") //load attributes from csv
+        .defer(d3.json, "data/state_province.topojson") //load choropleth spatial data
+        //.defer(d3.json, "data/states.topojson") //load background spatial data
         .await(callback);
 };
 
-function callback(error, csvData, states, tracts){
-    console.log(error);
-    console.log(csvData);
-    console.log(states);
-    console.log(tracts);
+function callback(error, csvData, states){
+  console.log("callback")
+    console.log("error",error);
+    console.log("csv",csvData);
+    console.log("states",states);
+    //console.log(tracts);
 };
-console.log("sup")
+console.log("done")
