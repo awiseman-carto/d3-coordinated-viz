b49d1fa61442a9ef29f13540b5a9cd408822d0e6
awiseman-carto <33461575+awiseman-carto@users.noreply.github.com>
2018-04-06 00:26:44 -0400

    getting rid of comments

--- js/main.js
+++ js/main.js
@@ -12,7 +12,6 @@ function setMap(){
     d3.queue()
         .defer(d3.csv, "data/state_province.csv") //load attributes from csv
         .defer(d3.json, "data/state_province.topojson") //load choropleth spatial data
-        //.defer(d3.json, "data/states.topojson") //load background spatial data
         .await(callback);
 };
 
@@ -23,6 +22,5 @@ function callback(error, csvData, states){
     console.log("error",error);
     console.log("csv",csvData);
     //console.log("states",states);
-    //console.log(tracts);
 };
 console.log("done")
