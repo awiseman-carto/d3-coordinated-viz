7bd4bc3d9eccdafb8c5d3d0945ca496fc58907dd
awiseman-carto <33461575+awiseman-carto@users.noreply.github.com>
2018-04-06 13:06:06 -0400

    adding another CSV

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 //begin script when window loads
-window.onload = function(){
+window.onload = setMap();
 
 // python -m SimpleHTTPServer the code for my dev server
 // C:\gis575\d3-coordinated-viz\d3-coordinated-viz>python -m SimpleHTTPServer
 // http://localhost:8000/index.html
 
 //set up choropleth map
-//function setMap(){
+function setMap(){
 
     //map frame dimensions
     var width = 960,
@@ -22,7 +22,7 @@ window.onload = function(){
     //create Albers equal area conic projection centered on France
     var projection = d3.geoAlbers()
         .center([0, 46.2])
-        //38, -97
+        //
         .rotate([-2, 0, 0])
         .parallels([43, 62])
         .scale(2500)
@@ -34,21 +34,21 @@ window.onload = function(){
 
     console.log("waiting")
     d3.queue()
-        .defer(d3.csv, "data/state_province.csv") //load attributes from csv
+        .defer(d3.csv, "data/tractdata.csv") //load attributes from csv
         .defer(d3.json, "data/state_province.topojson") //load choropleth spatial data
-        .defer(d3.json, "data/censustracts.topojson.json") //load choropleth spatial data
+        .defer(d3.json, "data/censustracts.topojson") //load choropleth spatial data
         .await(callback);
 //};
 
 function callback(error, csvData, states, census){
   console.log("callback")
   // translate to topojosn
-  var newState = topojson.feature(states, states.objects.state_province).features;
+  //var newState = topojson.feature(states, states.objects.state_province).features;
   var newCensus = topojson.feature(census, census.objects.censustracts).features;
     console.log("error",error);
-    console.log("csv",csvData);
-    console.log("states",states);
-    console.log("census",census);
+    console.log(csvData);
+    //console.log(states);
+    console.log(census);
     //console.log("states",states);
 
     var tracts = map.selectAll(".tracts")
